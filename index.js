39fcd30ff4c06f31a426af93cc06916f79bd012c
Rudra <merudramanu@gmail.com>
2023-10-06 00:48:38 +0530

    create user register and login routes with JWT implementation

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { connection } = require("./configs/db");
+const { userRouter } = require("./routes/user.route");
+
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+
+app.use("/home", (req, res) => {
+  res.send("APIs are working");
+});
+app.use("/user", userRouter);
+
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log("Connected to Database");
+  } catch (error) {
+    console.log("Error while connecting with Database");
+  }
+  console.log(`Server is running at port ${process.env.port}`);
+});
