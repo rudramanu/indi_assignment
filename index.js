adc2eafe0c790f97d463dfac56c61d3a1c35f304
Rudra <merudramanu@gmail.com>
2023-10-06 16:58:37 +0530

    resolving error

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const { swaggerServe, swaggerSetup } = require("./config");
 const cors = require("cors");
 const app = express();
 
-app.set("trust proxy", true);
 app.use(cors());
 app.use(express.json());
 require("dotenv").config();
